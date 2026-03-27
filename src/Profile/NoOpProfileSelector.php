74c7aeba9d2ef70ba41df6da1891ef06c9f3ade4
Daniel Dahme <danieldahme@googlemail.com>
2026-03-27 21:00:22 +0000

    header added

--- src/Profile/NoOpProfileSelector.php
+++ src/Profile/NoOpProfileSelector.php
@@ -1,5 +1,20 @@
 <?php declare(strict_types=1);
 
+/***********************************************************************
+ * This file is part of MissionBay for BASE3 Framework.
+ *
+ * MissionBay extends the BASE3 framework with a modular runtime
+ * foundation for agent flows, reusable nodes, and dockable resources.
+ * It provides declarative execution for AI-driven workflows.
+ *
+ * Developed by Daniel Dahme
+ * Licensed under GPL-3.0
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * https://base3.de/v/missionbay
+ * https://github.com/ddbase3/MissionBay
+ **********************************************************************/
+
 namespace MissionBay\Profile;
 
 use MissionBay\Api\IAgentContext;
